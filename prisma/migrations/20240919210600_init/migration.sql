3c71759e2d5d7ad0986777f54cd694b9017041f8
Lasun Alexandr <lasunaleksandr@gmail.com>
2024-09-20 00:06:31 +0300

    new migrate

--- prisma/migrations/20240919210600_init/migration.sql
+++ prisma/migrations/20240919210600_init/migration.sql
@@ -1,11 +1,14 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "socketId" TEXT,
     "email" TEXT NOT NULL,
     "user_name" TEXT,
     "password" TEXT NOT NULL,
     "color" JSONB NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "online" BOOLEAN NOT NULL DEFAULT false,
+    "lastSeen" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -28,6 +31,7 @@ CREATE TABLE "Message" (
 CREATE TABLE "Room" (
     "id" TEXT NOT NULL,
     "color" JSONB NOT NULL,
+    "owner" INTEGER NOT NULL,
     "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
